be11664aeeb5dd6364f9ece81092d4934fec5997
mabhay3420 <mishraabhay9179913420@gmail.com>
2025-10-17 21:51:09 +0530

    Validate split_data ratio bounds

--- include/data/split.hpp
+++ include/data/split.hpp
@@ -8,7 +8,9 @@
 template <typename T>
 void split_data(const float ratio, const std::vector<T>& data,
                 std::vector<T>& train_data, std::vector<T>& val_data) {
-  assert(ratio > 0.0f && ratio < 1.0f && "Ratio must be between 0 and 1");
+  if (!(ratio > 0.0f && ratio < 1.0f)) {
+    throw std::invalid_argument("split_data ratio must be in (0, 1)");
+  }
   auto split_index = static_cast<size_t>(data.size() * ratio);
   train_data.assign(data.begin(), data.begin() + split_index);
   val_data.assign(data.begin() + split_index, data.end());
